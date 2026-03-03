9be5d3b301d8613a2c9b8f534628c8b911a6837c
Jovic Gutierrez <jovicgutierrez@proton.me>
2026-03-03 12:28:21 +0800

    updated mmm fortran

--- SE02/mmm.f90
+++ SE02/mmm.f90
@@ -4,7 +4,7 @@ program matrix_multiplication
     integer :: n, trials, t
     real(8) :: start, finish, total_time
     integer :: block_size
-    n = 1500
+    n = 200
     trials = 30
     block_size = 16
 
@@ -24,7 +24,72 @@ subroutine benchmark_all(n, trials, block_size)
     call random_number(A)
     call random_number(B)
 
-    print *, "Running ijk..."
+    print *, "ORDERING IJK"
+    total = 0.0
+    do t = 1, trials
+        C = 0.0
+        call cpu_time(start)
+        call mmm_ijk(A,B,C,n)
+        call cpu_time(finish)
+        time = finish - start
+        total = total + (finish - start)
+        print *, time
+    end do
+    print *, "Average time:", total/trials
+
+    print *, "ORDERING IKJ"
+    total = 0.0
+    do t = 1, trials
+        C = 0.0
+        call cpu_time(start)
+        call mmm_ikj(A,B,C,n)
+        call cpu_time(finish)
+        time = finish - start
+        total = total + (finish - start)
+        print *, time
+    end do
+    print *, "Average time:", total/trials
+
+    print *, "ORDERING JIK"
+    total = 0.0
+    do t = 1, trials
+        C = 0.0
+        call cpu_time(start)
+        call mmm_jik(A,B,C,n)
+        call cpu_time(finish)
+        time = finish - start
+        total = total + (finish - start)
+        print *, time
+    end do
+    print *, "Average time:", total/trials
+
+    print *, "ORDERING JKI"
+    total = 0.0
+    do t = 1, trials
+        C = 0.0
+        call cpu_time(start)
+        call mmm_jki(A,B,C,n)
+        call cpu_time(finish)
+        time = finish - start
+        total = total + (finish - start)
+        print *, time
+    end do
+    print *, "Average time:", total/trials
+
+    print *, "ORDERING KIJ"
+    total = 0.0
+    do t = 1, trials
+        C = 0.0
+        call cpu_time(start)
+        call mmm_kij(A,B,C,n)
+        call cpu_time(finish)
+        time = finish - start
+        total = total + (finish - start)
+        print *, time
+    end do
+    print *, "Average time ijk:", total/trials
+
+    print *, "ORDERING KJI"
     total = 0.0
     do t = 1, trials
         C = 0.0
@@ -135,4 +200,29 @@ subroutine mmm_kji(A,B,C,n)
             end do
         end do
     end do
+end subroutine
+
+subroutine mmm_block(A,B,C,n,bs)
+    implicit none
+    integer, intent(in) :: n, bs
+    real(8), intent(in) :: A(n,n), B(n,n)
+    real(8), intent(inout) :: C(n,n)
+    integer :: i,j,k,ii,jj,kk
+
+    do ii = 1, n, bs
+        do jj = 1, n, bs
+            do kk = 1, n, bs
+
+                do i = ii, min(ii+bs-1,n)
+                    do j = jj, min(jj+bs-1,n)
+                        do k = kk, min(kk+bs-1,n)
+                            C(i,j) = C(i,j) + A(i,k)*B(k,j)
+                        end do
+                    end do
+                end do
+
+            end do
+        end do
+    end do
+
 end subroutine
